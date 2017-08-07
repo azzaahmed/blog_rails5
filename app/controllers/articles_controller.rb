3380a3ff4d335e3e40112041ba3c05cb9d841b39
azza ahmed <aazza_ahmed@hotmail.com>
2017-08-07 16:20:34 +0200

    Create Article successfully

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,21 @@
+class ArticlesController < ApplicationController
+  def index
+  end
+
+  def new 
+  	  @article = Article.new
+  end
+  def create
+@article = Article.new(article_params) 
+@article.save
+# flash is a hash with key success it should be viewed so we write a view in appliction.erb view body
+flash[:success] = "Article has been created" 
+redirect_to articles_path
+end
+
+# article_params is passed to new as it list info of article that is taken from the web while permiting title and body only
+private
+def article_params
+params.require(:article).permit(:title, :body) 
+end
+end

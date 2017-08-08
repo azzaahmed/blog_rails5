c726092cf219391d4f7eb3804d551359191327b5
azza ahmed <aazza_ahmed@hotmail.com>
2017-08-08 11:41:25 +0200

    flash.now when article falied to be created

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,15 +5,20 @@ class ArticlesController < ApplicationController
   def new 
   	  @article = Article.new
   end
+  
   def create
 @article = Article.new(article_params) 
 if @article.save
 # flash is a hash with key success it should be viewed so we write a view in appliction.erb view body
+#flash would be shown after the page is redirected to article path 
 flash[:success] = "Article has been created" 
 redirect_to articles_path
 else
-	flash[:danger] = "Article has not been created"
-render :new
+	#flash.now due to how flash works it would render new templete then display the flash 
+	#that will cause an issue if we typed the new path afterwards the flash wil be shown again
+	#so we need .now to display th e flash once
+	flash.now[:danger] = "Article has not been created"
+	render :new
 end
 end
 

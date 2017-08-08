55f6fe103d5d82ad4297f9cd91b5dff44850d56f
azza ahmed <aazza_ahmed@hotmail.com>
2017-08-08 12:11:30 +0200

    implement listing articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,12 @@
 class ArticlesController < ApplicationController
   def index
+ @articles = Article.all
   end
 
   def new 
   	  @article = Article.new
   end
-  
+
   def create
 @article = Article.new(article_params) 
 if @article.save

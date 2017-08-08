61f270074ddb689c8f4814f2b76ed02a6069e55f
azza ahmed <aazza_ahmed@hotmail.com>
2017-08-08 11:11:46 +0200

    Article validations and flash message

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,10 +7,14 @@ class ArticlesController < ApplicationController
   end
   def create
 @article = Article.new(article_params) 
-@article.save
+if @article.save
 # flash is a hash with key success it should be viewed so we write a view in appliction.erb view body
 flash[:success] = "Article has been created" 
 redirect_to articles_path
+else
+	flash[:danger] = "Article has not been created"
+render :new
+end
 end
 
 # article_params is passed to new as it list info of article that is taken from the web while permiting title and body only

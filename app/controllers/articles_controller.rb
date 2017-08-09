5a18935434ee88aa4fb16b0d365b1e6430f55d8c
azza ahmed <aazza_ahmed@hotmail.com>
2017-08-09 13:16:45 +0200

    Restrict access with controller actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -33,18 +33,25 @@ def show
 end
 
 def edit
-
+ unless @article.user == current_user
+      flash[:alert] = "You can only edit your own article."
+      redirect_to root_path
+    end
 end
 
 def update
 
-if @article.update(article_params)
-flash[:success] = "Article has been updated"
-# redirect to show article 
-redirect_to @article 
-else
-flash.now[:danger] = "Article has not been updated"
-render :edit
+   unless @article.user == current_user
+       flash[:danger] = "You can only edit your own article."
+       redirect_to root_path
+     else  
+       if @article.update(article_params)
+         flash[:success] = "Article has been updated"
+         redirect_to @article
+       else
+         flash.now[:danger] = "Article has not been updated"
+         render :edit
+       end
 end
 end
 
